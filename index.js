fa7126efe3a0f1fa19b1f6943e3d6be5880866ed
Rocco Musolino <rocco@hackerstribe.com>
2015-12-11 05:04:57 +0100

    Introduced periodic check.

--- index.js
+++ index.js
@@ -10,27 +10,49 @@ function execute(command, callback){ // Execute CLI cmds
     exec(command, function(error, stdout, stderr){ callback(stdout); });
 };
 
-
+function parse_temp(data){
+	return data.substr(data.indexOf('=')+1, 4);
+}
 
 
 
 var pin = new Gpio(config.PIN_NUMBER, 'out'); // Check out RPi2 Pinout map
 
-//pin.writeSync(1); // ON
-//pin.writeSync(0); // OFF
 
+// Start Loop
+(
+function loop(){
+	var iter = setInterval(function(){
+		execute('/opt/vc/bin/vcgencmd measure_temp', function(data){
+			if (parse_temp(data) >= config.TEMPERATURE_THRESHOLD){
+				pin.read(function(err, value){
+					if (value == 0){ 
+						pin.writeSync(1); // ON
+						console.log(new Date().toString(), ' - Fan ON!');
+					}
+				});		
+				clearInterval(iter);
+				setTimeout(loop, 300 * 1000); // Fan on at least for 5 minutes 
+			}else{
+				pin.read(function(err, value){
+					if (value == 1){
+						pin.writeSync(0); // OFF
+						console.log(new Date().toString(), ' - Fan OFF!');
+					}
+				});
+			}
+		});
+	}, config.REFRESH_TIME * 1000);
+}()
+);
 
-
-
-function parse_temp(data){
-	return data.substr(data.indexOf('=')+1, 4);
-}
+// Web Server
 
 if (config.WEB_SERVER){
 	var http = require('http');
 
 	var server = http.createServer(function (req, res) {
-	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  res.writeHead(200, {'Content-Type': 'text/html'});
 	  
 	  execute('/opt/vc/bin/vcgencmd measure_temp', function(data){
 	  	 pin.read(function(err, val){
@@ -41,9 +63,9 @@ if (config.WEB_SERVER){
 	  
 	})
 
-	server.listen(4949, '127.0.0.1');
+	server.listen(config.SERVER_PORT, '0.0.0.0');
 
-	console.log('Server running at http://127.0.0.1:4949/');
+	console.log('Fan server running at http://0.0.0.0:'+config.SERVER_PORT+'/');
 }
 
 
